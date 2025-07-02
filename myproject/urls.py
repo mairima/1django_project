5323a05cd48bec3c8717febc64a675a707d6a7b2
mairima <aissatou.mairi@gmail.com>
2025-07-02 09:53:17 +0200

    Add heroku

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from hello_world import views as index_views
-urlpatterns = [
-    path('', index_views.index, name='index'),
-    path('admin/', admin.site.urls),
-]
+from django.urls import path
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("🚀 Django is working! This is your homepage.")
+
+urlpatterns = [
+    path("", home),  # add this line to handle "/"
+    path("admin/", admin.site.urls),
+]
